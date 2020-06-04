f41f606d6daece80e2d417a2d93e48e9cd8a4e9f
webjasiek <53037344+webjasiek@users.noreply.github.com>
2020-06-04 19:54:51 +0200

    Update 003.php

--- 003.php
+++ 003.php
@@ -3,33 +3,32 @@
 /** drow `line` ;) */
 function hr() {    echo "\n".str_repeat("_",55) . "\n";  }  # element estetyczny
 /**
-browse() is my version of displaying everything (almots)
-@param mixed $a =wartość do wyświetlenia
-@param int $method = 0|1|2 print_r, var_export, var_dump: default 1
-@return not return
-Example: browse($var); browse([$var1,$var2],0);
+ * browse($var,$method) is my version of displaying everything (almots)
+ * @param mixed var = variable for analysis and display
+ * @param int method = 0|1|2 functions: print_r(0) or var_export(1) or var_dump(2), default 1
+ * @author Tomasz Jaśniewski <biuro@webjasiek.pl>
 */
-function browse($a,$method=1) {
+function browse($var,$method=1) {
    hr();
    if ($method==2) {
-      var_dump($a);
+      var_dump($var);
       return;
    }
    $m = ($method==1) ? "var_export" : "print_r";
-   if (is_array($a) and count($a) and $method!=2) {
+   if (is_array($var) and count($var) and $method!=2) {
       while (true) {
-         $e = current($a);
+         $e = current($var);
          if ($method==0)
-            echo str_replace("\n"," ", key($a)."=>[".$m($e,true)."]");
+            echo str_replace("\n"," ", key($var)."=>[".$m($e,true)."]");
          else
             echo str_replace("\n"," ", $m($e,true));
-         if (next($a)!==FALSE) echo ", ";
-         else {reset($a); break; }
+         if (next($var)!==FALSE) echo ", ";
+         else {reset($var); break; }
       }
       echo "\n" . str_repeat("-",55);
    }
    else
-      echo preg_replace("/[\n\r\f]+/"," ", $m($a,true)) . "\n" . str_repeat("-",55);
+      echo preg_replace("/[\n\r\f]+/"," ", $m($var,true)) . "\n" . str_repeat("-",55);
    echo "\n";
 }
 ////////////////////////////////////////////////////////////////////////////////////
