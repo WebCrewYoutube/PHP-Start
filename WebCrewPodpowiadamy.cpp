e1c024785be29760338a1330cc9b712b39ed5d94
webjasiek <53037344+webjasiek@users.noreply.github.com>
2020-06-09 18:29:06 +0200

    new file added
    dodano kilka plików, które posłużą za pomoc w razie szybkiego pokazania kodu dla widzów

---
+++ WebCrewPodpowiadamy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   cout << "Jaś i Małgosia kupili stworzonka.\n
+   Jaś wybrał szczypawkę a Małgosia kreta.\n
+   Wiersze się rymują, zjedz proszę kotleta.\n";
+
+   return 1;
+}
