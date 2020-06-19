c65050005e4ee223710caf6f4422cfc13b73d879
webjasiek <53037344+webjasiek@users.noreply.github.com>
2020-06-19 20:38:10 +0200

    014 update

--- 014 test/014.php
+++ 014 test/014.php
@@ -1,8 +1,6 @@
 <?php
 $loader = require __DIR__ . '/vendor/autoload.php';
-
 use webcrew\{Test\AClass as A, BClass as B};
-
 $a = new A;
 $b = new B;
 e(1,2,3);
---
+++ 014 test/src/BClass.php
@@ -0,0 +1,10 @@
+<?php
+namespace webcrew;
+class BClass {
+	function __construct() {
+		echo get_called_class(),PHP_EOL;
+	}
+}
+
+
+?>
