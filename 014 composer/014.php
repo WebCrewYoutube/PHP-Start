1594c89b08563989bf60f036916a8d774210a823
webjasiek <53037344+webjasiek@users.noreply.github.com>
2020-06-19 13:18:45 +0200

    014 update 2

--- 014 composer/014.php
+++ 014 composer/014.php
@@ -1,10 +1,11 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+$loader = require __DIR__ . '/vendor/autoload.php';
 use webcrew\{Test\AClass as A, BClass as B};
 $a = new A;
 $b = new B;
 e(1,2,3);
-
+hr('#');
+browse($loader,2);
 
 
 
